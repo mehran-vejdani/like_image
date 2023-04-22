74b6ba00425a2da1c411e2c72034adfe76bf95a8
“mehran <mehran.vejdanii@gmail.com>
2023-04-22 15:06:20 +0430

    make the position with the target offset and show the heart where the  user clicked

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const image = document.querySelector(".container");
 const heartIcon = document.querySelector(".heart");
 
-image.addEventListener("dblclick ", (e) => {
+image.addEventListener("dblclick", (e) => {
+  //set the position of ther heart using x and y value
+  let xvalue = e.clientX - e.target.offsetLeft;
+  let yvalue = e.clientY - e.target.offsetTop;
+  console.log(xvalue, yvalue);
+
+  heartIcon.style.left = `${xvalue}px`;
+  heartIcon.style.top = `${yvalue}px`;
+
   heartIcon.classList.add("active");
+
+  setTimeout(() => {
+    heartIcon.classList.remove("active");
+  }, 1000);
 });
