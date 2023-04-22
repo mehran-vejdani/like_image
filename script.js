41f4f91cc3f7afeebd09763f87e3852acdc47e72
“mehran <mehran.vejdanii@gmail.com>
2023-04-22 14:38:46 +0430

    add dblclick event and make animattion in css

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const image = document.querySelector(".container");
-const heart = document.querySelector(".heart");
+const heartIcon = document.querySelector(".heart");
+
+image.addEventListener("dblclick ", (e) => {
+  heartIcon.classList.add("active");
+});
